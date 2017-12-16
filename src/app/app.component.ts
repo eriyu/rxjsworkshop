0aafdd1ea3a690bda1148fc46c80a4bf1233ba11
拉拉醬 <lala.lee.goo@gmail.com>
2017-12-16 10:48:31 +0800

    form validate simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
-import { NgControl } from "@angular/forms";
+import { NgControl, FormGroup } from "@angular/forms";
 import { Subject } from "rxjs/Subject";
 
 import { takeUntil } from 'rxjs/operators';
@@ -10,31 +10,8 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./app.component.css'],
   styles: []
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('searchRef') searchControl: NgControl;
-  sub;
-  destroy$ = new Subject();
-
-  ngAfterViewInit() {
-    console.log(this.searchControl);
-    // this.sub = this.searchControl.valueChanges.subscribe(value => {
-    //   console.log(value);
-    // });
-    this.searchControl.valueChanges
-      .pipe(
-        takeUntil(this.destroy$)
-      ).subscribe(value => console.log(value));
-  }
-
-  ngOnDestroy() {
-    // 1: use sub
-    // this.sub.unsubscribe();
-
-    // 2: use subject takeUntil
-    // this.destroy$.next('');
-    // this.destroy$.unsubscribe();
-
-    // 2: use subject takeUntil or use complete
-    this.destroy$.complete();
+export class AppComponent {
+  send(form) {
+    console.log(form);
   }
 }
